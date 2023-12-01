0f1b2d2124a41afcefb27ae44906c50730495eb2
Marcos Vinicius <85797701+strngrthnall@users.noreply.github.com>
2023-11-30 22:19:26 -0300

    Troca no icone do github

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,7 +8,6 @@ import {
   gridContainer2
 } from './Header.module.css'
 
-const githubIconLink = 'https://raw.githubusercontent.com/strngrthnall/React-IMC/main/src/assets/github.svg'
 const githubIconPath = '../../assets/github.svg'
 
 const Header = () => {
@@ -26,7 +25,6 @@ const Header = () => {
           <a href="https://github.com/strngrthnall/React-IMC">
             <img
             src={ githubIconPath }
-            srcSet={ githubIconLink  }
             className={ githubIcon }
           />
           </a>
